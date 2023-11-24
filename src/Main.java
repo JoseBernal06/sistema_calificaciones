9514a9c168969f75f7d39a4a7c7375b26767018c
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2023-11-24 12:00:16 -0500

    Septima version

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         Estudiante estudiante_uno, estudiante_dos;
         estudiante_uno=new Estudiante("Mateo", "Bernal","Redes", "Sistemas","POO","Algoritmos","Gestion",15.25, 12);
+        estudiante_dos=new Estudiante("Emilio", "Erazo","Redes", "Sistemas","POO","Algoritmos","Gestion",17.80, 15.60);
 
 
 
